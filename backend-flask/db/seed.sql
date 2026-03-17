22ef83bd156b273348e9f0efb2db5eca9fd0a710
oXbLiXxX <97739416+oxblixxx@users.noreply.github.com>
2026-03-17 19:29:39 +0000

    ♻️: refactor
    REFACTOR SYNTAX

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,14 +1,14 @@
--- Insert users with required email field
-INSERT INTO public.users (display_name, handle, email, cognito_user_id)
+-- this file was manually created
+INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown', 'andrew@example.com', 'MOCK'),
-  ('Andrew Bayko', 'bayko', 'bayko@example.com', 'MOCK');
+  ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
+  ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK'),
+  ('Londo Mollari', 'lmollari@centari.com','londo','MOCK');
 
--- Insert activity linked to Andrew Brown
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid FROM public.users WHERE handle = 'andrewbrown' LIMIT 1),
-    'This was imported as seed data for prod!',
+    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    'This was imported as seed data!',
     current_timestamp + interval '10 day'
-  );
+  )
