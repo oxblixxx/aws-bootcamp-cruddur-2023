754fb90cda2d090a711be274c7b7a0f5d37d1000
oXbLiXxX <97739416+oxblixxx@users.noreply.github.com>
2026-03-22 21:37:14 +0000

    ✨: SETUP COMPONENTS FOR SENDING MESSAGE AND REFACTORED CONNECTING COMPONENTS

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 
 import HomeFeedPage from './pages/HomeFeedPage';
-import NotificationFeedPage from './pages/NotificationsFeedPage';
+import NotificationsFeedPage from './pages/NotificationsFeedPage';
 import UserFeedPage from './pages/UserFeedPage';
 import SignupPage from './pages/SignupPage';
 import SigninPage from './pages/SigninPage';
 import RecoverPage from './pages/RecoverPage';
 import MessageGroupsPage from './pages/MessageGroupsPage';
 import MessageGroupPage from './pages/MessageGroupPage';
+import MessageGroupNewPage from './pages/MessageGroupNewPage';
 import ConfirmationPage from './pages/ConfirmationPage';
 import React from 'react';
 import {
@@ -22,18 +23,16 @@ Amplify.configure({
   "aws_cognito_region": process.env.REACT_APP_AWS_COGNITO_REGION,
   "aws_user_pools_id": process.env.REACT_APP_AWS_USER_POOLS_ID,
   "aws_user_pools_web_client_id": process.env.REACT_APP_CLIENT_ID,
-  //"aws_user_pools_client_secret": process.env.REACT_APP_WEB_CLIENT_SECRET,
   "oauth": {},
   Auth: {
     // We are not using an Identity Pool
     // identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID, // REQUIRED - Amazon Cognito Identity Pool ID
-    region: process.env.REACT_AWS_PROJECT_REGION,           // REQUIRED - Amazon Cognito Region
+    region: process.env.REACT_APP_AWS_PROJECT_REGION,           // REQUIRED - Amazon Cognito Region
     userPoolId: process.env.REACT_APP_AWS_USER_POOLS_ID,         // OPTIONAL - Amazon Cognito User Pool ID
     userPoolWebClientId: process.env.REACT_APP_CLIENT_ID,   // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
-    //userPoolClientSecret: process.env.REACT_APP_WEB_CLIENT_SECRET,
   }
 });
-  
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -41,7 +40,7 @@ const router = createBrowserRouter([
   },
   {
     path: "/notifications",
-    element: <NotificationFeedPage />
+    element: <NotificationsFeedPage />
   },
   {
     path: "/@:handle",
@@ -52,7 +51,11 @@ const router = createBrowserRouter([
     element: <MessageGroupsPage />
   },
   {
-    path: "/messages/@:handle",
+    path: "/messages/new/:handle",
+    element: <MessageGroupNewPage />
+  },
+  {
+    path: "/messages/:message_group_uuid",
     element: <MessageGroupPage />
   },
   {
@@ -80,4 +83,5 @@ function App() {
     </>
   );
 }
-export default App; 
+
+export default App;
--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -1,21 +1,12 @@
 import './HomeFeedPage.css';
 import React from "react";
 
-
 import DesktopNavigation  from '../components/DesktopNavigation';
 import DesktopSidebar     from '../components/DesktopSidebar';
 import ActivityFeed from '../components/ActivityFeed';
 import ActivityForm from '../components/ActivityForm';
 import ReplyForm from '../components/ReplyForm';
-
-import { Auth } from 'aws-amplify';
-
-// [TODO] Authenication
-// import Cookies from 'js-cookie'
-
-//Honeycomb Tracing
-import { trace, context, } from '@opentelemetry/api';
-const tracer = trace.getTracer();
+import checkAuth from '../lib/CheckAuth';
 
 export default function HomeFeedPage() {
   const [activities, setActivities] = React.useState([]);
@@ -28,62 +19,32 @@ export default function HomeFeedPage() {
   const loadData = async () => {
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/home`
-      var startTime = performance.now() //capture start time
       const res = await fetch(backend_url, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`
+        },
         method: "GET"
       });
-      var endTime = performance.now() //capture when result was returned
       let resJson = await res.json();
       if (res.status === 200) {
         setActivities(resJson)
-      //Start custom span
-      tracer.startActiveSpan('HomeFeedPageLoadSpan', hmfSpan => {
-        // Add attributes to custom span
-        hmfSpan.setAttribute('homeeFeedPage.latency_MS', (endTime - startTime)); //Latency in milliseconds
-        hmfSpan.setAttribute('homeeFeedPage.status', true); //status of the item retrieved
-        hmfSpan.end();
-      });        
       } else {
         console.log(res)
-         // same as above but for when the response isnt a success
-         tracer.startActiveSpan('HomeFeedPageLoadSpan', hmfSpan => {
-          hmfSpan.setAttribute('homeeFeedPage.latency_MS', (endTime - startTime));
-          hmfSpan.setAttribute('homeeFeedPage.status', false);
-          hmfSpan.end();
-        });
       }
     } catch (err) {
       console.log(err);
     }
   };
 
-  // check if we are authenicated
-const checkAuth = async () => {
-  Auth.currentAuthenticatedUser({
-    // Optional, By default is false. 
-    // If set to true, this call will send a 
-    // request to Cognito to get the latest user data
-    bypassCache: false 
-  })
-  .then((user) => {
-    console.log('user',user);
-    return Auth.currentAuthenticatedUser()
-  }).then((cognito_user) => {
-      setUser({
-        display_name: cognito_user.attributes.name,
-        handle: cognito_user.attributes.preferred_username
-      })
-  })
-  .catch((err) => console.log(err));
-};
 
+  
   React.useEffect(()=>{
     //prevents double call
     if (dataFetchedRef.current) return;
     dataFetchedRef.current = true;
 
     loadData();
-    checkAuth();
+    checkAuth(setUser);
   }, [])
 
   return (
@@ -91,7 +52,6 @@ const checkAuth = async () => {
       <DesktopNavigation user={user} active={'home'} setPopped={setPopped} />
       <div className='content'>
         <ActivityForm  
-          user_handle={user}
           popped={popped}
           setPopped={setPopped} 
           setActivities={setActivities} 
