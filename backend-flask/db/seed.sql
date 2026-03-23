7e97df155a08a1c8d80fa4510dfe80aff515c481
oXbLiXxX <97739416+oxblixxx@users.noreply.github.com>
2026-03-23 23:38:02 +0000

    ADDED COMMENT

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,4 +1,4 @@
--- this file was manually created
+-- this file was manually created, replace the values with corresponding one from AWS COGNITO USER POOL FROM WEEK 4
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES  
   ('Pelumi Mustapha','phamust1111@gmail.com' , 'oxblixxxx' ,'MOCK'),
@@ -8,6 +8,7 @@ VALUES
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
+-- replace with users.handle name with the primary user you'd need to login with
   (
     (SELECT uuid from public.users WHERE users.handle = 'oxblixxxx' LIMIT 1),
     'This was imported as seed data!',
