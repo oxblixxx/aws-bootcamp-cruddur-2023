19589cf54539b74f9d7ab4245f08767091acdce9
oXbLiXxX <97739416+oxblixxx@users.noreply.github.com>
2026-03-23 11:00:58 +0000

    ✨: IMPLEMENT BACKEND TO INTERACT WITH DATABASE TO CREATE MESSAGE AND CHAT WITH USERS

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,6 +2,8 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES  
   ('Pelumi Mustapha','phamust1111@gmail.com' , 'oxblixxxx' ,'MOCK'),
+  ('Londo Mollari', 'lmollari@centari.com','londo','MOCK'),
+  ('Lundi Madu','torontoqw1@gmail.com' , 'toronto' ,'MOCK'),
   ('Pelumi Mustapha','oxblixxx@gmail.com' , 'bigtade' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
