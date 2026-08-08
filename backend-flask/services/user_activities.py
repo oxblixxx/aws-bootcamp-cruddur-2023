7c5f6f77e795cdfa278196576f636d45b52ab8da
oxblixxx <phamust1111@gmail.com>
2026-08-08 01:07:09 +0000

    feat: add SQL query for user profile and activities retrieval

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,23 +1,28 @@
-from datetime import datetime, timedelta, timezone
+from lib.db import db
+#from aws_xray_sdk.core import xray_recorder
 class UserActivities:
   def run(user_handle):
+    #try:
     model = {
       'errors': None,
       'data': None
     }
-
-    now = datetime.now(timezone.utc).astimezone()
-
     if user_handle == None or len(user_handle) < 1:
       model['errors'] = ['blank_user_handle']
     else:
-      now = datetime.now()
-      results = [{
-        'uuid': 'e4187488-d031-70c2-95b1-1c460629bb48',
-        'handle':  'Pelumi Mustapha',
-        'message': 'Cloud is fun!',
-        'created_at': (now - timedelta(days=1)).isoformat(),
-        'expires_at': (now + timedelta(days=31)).isoformat()
-      }]
+      print("else:")
+      sql = db.template('users','show')
+      results = db.query_object_json(sql,{'handle': user_handle})
       model['data'] = results
+      #subsegment = xray_recorder.begin_subsegment('mock-data')
+      ## xray ---
+      #dict = {
+      #  "now": now.isoformat(),
+      #  "results-size": len(model['data'])
+      #}
+      #subsegment.put_metadata('key', dict, 'namespace')
+      #xray_recorder.end_subsegment()
+    #finally:  
+    ##  # Close the segment
+    #  xray_recorder.end_subsegment()
     return model
