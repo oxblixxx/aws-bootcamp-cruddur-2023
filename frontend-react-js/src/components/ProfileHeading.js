32200e720c035411c3612ed6700f64476358fb44
oxblixxx <phamust1111@gmail.com>
2026-08-14 20:28:15 +0000

    feat: implement S3 file upload functionality in ProfileForm component

--- frontend-react-js/src/components/ProfileHeading.js
+++ frontend-react-js/src/components/ProfileHeading.js
@@ -2,49 +2,30 @@ import './ProfileHeading.css';
 import EditProfileButton from '../components/EditProfileButton';
 
 export default function ProfileHeading(props) {
-  const backgroundImage =
-    'url("https://assets.mustaphaops.online/avatars/lemon.jpg")';
-
+  const backgroundImage = 'url("https://assets.mustaphaops.online/avatars/lemon.jpg")';
   const styles = {
-    backgroundImage,
+    backgroundImage: backgroundImage,
     backgroundSize: 'cover',
     backgroundPosition: 'center',
   };
-
   return (
-    <div className="activity_feed_heading profile_heading">
-      <div className="title">{props.profile.display_name}</div>
-
-      <div className="cruds_count">
-        {props.profile.cruds_count} Cruds
-      </div>
-
-      <div className="banner" style={styles}>
-        <div className="avatar">
-          <img
-            src="https://assets.mustaphaops.online/avatars/lemon.jpg"
-            alt="Profile"
-          />
-        </div>
-      </div>
-
-      <div className="info">
-        <div className="id">
-          <div className="display_name">
-            {props.profile.display_name}
-          </div>
-
-          <div className="handle">
-            @{props.profile.handle}
-          </div>
-        </div>
-
-        <EditProfileButton setPopped={props.setPopped} />
-
-        <div className="bio">
-          {props.profile.bio}
-        </div>
+  <div className='activity_feed_heading profile_heading'>
+    <div className='title'>{props.profile.display_name}</div>
+    <div className="cruds_count">{props.profile.cruds_count} Cruds</div>
+    <div className="banner" style={styles} >
+      <div className="avatar">
+        <img src="https://assets.mustaphaops.online/avatars/lemon.jpg"></img>
       </div>
     </div>
+    <div className="info">
+      <div className='id'>
+        <div className="display_name">{props.profile.display_name}</div>
+        <div className="handle">@{props.profile.handle}</div>
+      </div>
+      <EditProfileButton setPopped={props.setPopped} />
+    </div>
+    <div className="bio">{props.profile.bio}</div>
+
+  </div>
   );
 }
